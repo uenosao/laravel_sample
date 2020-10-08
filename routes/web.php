3abd09b1e86abb665cc86ae096bb96f769dccc8a
uenosao <ariii0312sa@gmail.com>
2020-10-08 20:20:49 +0900

    first commit

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('hello/{msg}',function ($msg) {
+
+$html = <<<EOF
+<html>
+<head>
+<title>Hello</title>
+<style>
+body {font-size:16pt; color:#999;}
+h1 { font-size:100pt;text-align:right;color:#eee;
+    margin:−40px 0px -50px 0px;}
+</style>
+</head>
+<body>
+    <h1>Hello</h1>
+    <p>{$msg}</p>
+    <p>これは、サンプルで作ったページです。</p>
+</body>
+</html>
+EOF;
+
+    // Route::get('hello',function () use ($html) {
+         return $html;
+    });
+
+
+    // Route::get(function () {
+    // return view('welcome');
+// });
+
+
