c1430c9f76e4e4f98b3b1b49ffe470e488045aba
uenosao <ariii0312sa@gmail.com>
2020-10-15 20:59:23 +0900

    new web file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers; 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,26 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 // */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-// $html = <<<EOF
-// <html>
-// <head>
-// <title>Hello</title>
-// <style>
-// body {font-size:16pt; color:#999;}
-// h1 { font-size:100pt;text-align:right;color:#eee;
-//     margin:40px 0px -50px 0px;}
-//     </style>
-//     </head>
-//     <body>
-//         <h1>Hello</h1>
-//         <p>This is sample page.</p>
-//         <p>これは、サンプルで作ったページです。</p>
-//     </body>
-//     </html>
-// EOF;
-
-    Route::get('hello','HelloController@index');
+// Route::get('hello', [HelloController::class, 'index']);
+Route::get('hello','HelloController@index');
 
     // Route::get('hello',function () use ($html) {
     //     return $html;
