7537d4d8a583aca9534188b56850bfbe43b4ab36
uenosao <ariii0312sa@gmail.com>
2020-10-17 22:09:14 +0900

    new_file76

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,13 +5,26 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class HelloController extends Controller
-{    public function index() {
-
-    $data = [
-            'msg'=>'これはBladeを利用したサンプルです。'
-];
-    return view('hello.index',$data);
+{    
+    
+    public function index() 
+ {
+//     $data = [
+//             'msg'=>'お名前を入力してください。'
+// ];
+    return view('hello.index');
 }
+
+public function post(Request $request)
+{
+    // $msg = $request->msg;
+    // $data = [
+    //     'msg' => 'こんにちは、' . $msg . 'さん！'
+    // ];
+    return view('hello.index',['msg' => $request -> msg]);
+    }
+}    
+
 // <html>
 // <head>
 // <title>Hello/Index</title>
@@ -29,4 +42,3 @@ class HelloController extends Controller
 // EOF;
 
 //     }
-} 
