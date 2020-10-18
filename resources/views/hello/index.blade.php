494d8678f93fe246de0622742d9a86fcfdc88273
uenosao <ariii0312sa@gmail.com>
2020-10-18 10:27:29 +0900

    new file79

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,11 +9,17 @@
 </head>
 <body>
     <h1>Blade/Index</h1>
-    <p>&#064;foreachディレクティブの例</p>
+    <p>&#064;whileディレクティブの例</p>
     <ol>
-    @foreach($data as $item)
-    <li>{{$item}}
-    @endforeach
+    @php
+    $counter = 0;
+    @endphp
+    @while($counter < count($data))
+    <li>{{$data[$counter]}}</li>
+    @php
+    $counter++;
+    @endphp
+    @endwhile
     </ol>
 </body>
 </html>
